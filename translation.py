62db4fbd08104a0f80aabbfe4b1724d52776933c
Guilherme Diel <guiddiel@gmail.com>
2022-11-07 17:47:27 -0300

    missing profile

--- translation.py
+++ translation.py
@@ -3,32 +3,71 @@ import json
 
 def translate(in_file, out_file):
 
-    nb_res = 128
+    nb_res = {"nb_res": 128}
     jobs = []
-    profiles = []
-
-
+    profiles = {"profiles": 
+        {
+            "simple": {
+                "type": "parallel",
+                "cpu": [5e6,  0,  0,  0],
+                "com": [5e6,  0,  0,  0,
+                        5e6,5e6,  0,  0,
+                        5e6,5e6,  0,  0,
+                        5e6,5e6,5e6,  0]
+            },
+            "homogeneous": {
+                "type": "parallel_homogeneous",
+                "cpu": 10e6,
+                "com": 1e6
+            },
+            "homogeneous_no_cpu": {
+                "type": "parallel_homogeneous",
+                "cpu": 0,
+                "com": 1e6
+            },
+            "homogeneous_no_com": {
+                "type": "parallel_homogeneous",
+                "cpu": 2e5,
+                "com": 0
+            },
+            "sequence": {
+                "type": "composed",
+                "repeat" : 4,
+                "seq": ["simple","homogeneous","simple"]
+            },
+            "delay": {
+                "type": "delay",
+                "delay": 20.20
+            },
+            "homogeneous_total": {
+                "type": "parallel_homogeneous_total",
+                "cpu": 10e6,
+                "com": 1e6
+            }
+        }
+    }
 
     with open(in_file, 'r') as f:
         for l in f:
-            line = []
             if l.split()[0] != ';':
-                job = {'id': int(l.split()[0]), 'profile': '{}', 'res': int(l.split()[7]), 'subtime': int(l.split()[1]), 'walltime': int(l.split()[3])}
+                job = {'id': int(l.split()[0]), 'profile': 'parallel_homogeneous', 'res': int(l.split()[7]), 'subtime': int(l.split()[1]), 'walltime': int(l.split()[3])}
                 jobs.append(job)
 
-    final = """{"description": "This workload is part of those which have been generated to conduct the experiments described in Batsim's JSSPP article. More information about how it has been generated can be found in the article and on the Batsim Experiments github page (https://github.com/oar-team/batsim-experiments)",
-    "command": "translate_submission_times.py -i 4 -w generated_workloads/2016-05-04/g5k_workload_delay_seed1_size32.json",
+    final = """{\n"description": "This workload is part of those which have been generated to conduct the experiments described in Batsim's JSSPP article. More information about how it has been generated can be found in the article and on the Batsim Experiments github page (https://github.com/oar-team/batsim-experiments)",
+    \n  "command": "translate_submission_times.py -i 4 -w generated_workloads/2016-05-04/g5k_workload_delay_seed1_size32.json",
     "date": "2016-05-17 10:28:31.851083"}"""
 
     test = json.loads(final)
     job_final = {"jobs": jobs}
     test.update(job_final)
+    test.update(nb_res)
+    test.update(profiles)
 
 
 
     
     with open(out_file, 'w') as f:
-        json.dump(test, f)
+        json.dump(test, f, indent=2, sort_keys=True)
 
 
 
