7b1a0043e85ddc55c2c84d10dcdf2f6e0a2965f8
guilhermedd <guiddiel@gmail.com>
2022-11-16 14:17:00 -0300

    finished

--- translation.py
+++ translation.py
@@ -9,6 +9,8 @@ def translate(in_file, out_file):
     num_profile = 0
     answer = -1
     FLOP_INFRASTRUCTURE = 22355
+    base = "{}"
+    profiles = json.loads(base)
 
     while answer < 1 or answer > 2:
         print("Would you like to use Walltime (press 1) or runtime (press 2)?")
@@ -18,25 +20,30 @@ def translate(in_file, out_file):
     with open(in_file, 'r') as f: # file with the workload
         for l in f:
             if l.split()[0] != ';': # ignores comments
+                if answer == 1 and (int(l.split()[7])  == -1 or float(l.split()[5]) == -1 or int(l.split()[0]) == -1 or int(l.split()[1]) == -1 or int(l.split()[8]) == -1) :
+                    continue
+                if answer != 1 and (int(l.split()[7])  == -1 or float(l.split()[5]) == -1 or int(l.split()[0]) == -1 or int(l.split()[1]) == -1 or int(l.split()[3]) == -1) :
+                    continue
                 jobs.append({'id': int(int(l.split()[0]) if int(l.split()[0]) != -1 else 0), 'profile': 'profile_job_{}'.format(num_profile), 'res': int(int(l.split()[7]) if int(l.split()[7]) != -1 else 0), 'subtime': int(int(l.split()[1]) if int(l.split()[1]) != -1 else 0), 'walltime': int(int(l.split()[8]) if int(l.split()[8]) != -1 else 0)} if answer == 1 else {
                             'id': int(int(l.split()[0]) if int(l.split()[0]) != -1 else 0), 'profile': 'profile_job_{}'.format(num_profile), 'res': int(int(l.split()[7]) if int(l.split()[7]) != -1 else 0), 'subtime': int(int(l.split()[1]) if int(l.split()[1]) != -1 else 0), 'walltime': int(int(l.split()[3]) if int(l.split()[3]) != -1 else 0)})
 
-                profiles.append({'profile_job_{}'.format(num_profile): {'type': 'parallel_homogeneous', 'cpu': int(int(l.split()[7]))*float(l.split()[5])*FLOP_INFRASTRUCTURE if int(l.split()[7]) and float(l.split()[5]) != -1 else 0, 'com': 0}})
+                profiles.update({'profile_job_{}'.format(num_profile): {'type': 'parallel_homogeneous', 'cpu': int(int(l.split()[7]))*float(l.split()[5])*FLOP_INFRASTRUCTURE}})
                 num_profile += 1
 
     final = """{"description": "This workload is part of those which have been generated to conduct the experiments described in Batsim's JSSPP article. More information about how it has been generated can be found in the article and on the Batsim Experiments github page (https://github.com/oar-team/batsim-experiments). Infrastructure details: Intel Xeon E5-2698 v4 @ 2.20GHz ==> 22.355 FLOPS",
     "command": "python3 translation.py workload_file_input json_file_output",
     "date": "03-11-2022 11:30:25"}"""
 
+    profiles_final = {"profile": profiles}
     updated = json.loads(final)
     final_jobs = {"jobs": jobs}
     final_profiles = {"profiles":  profiles}
     updated.update(final_jobs)
     updated.update(nb_res)
-    updated.update(final_profiles)
+    updated.update(profiles_final)
 
     with open(out_file, 'w') as f: # json file
-        json.dump(updated, f, indent=2)
+        json.dump(updated, f, indent=4)
 
 #             "id": 0,                      -- name
 #             "profile": "delay_ft.B.1",    --
@@ -53,6 +60,7 @@ if __name__ == '__main__':
     translate(arg.input_file, arg.json_file)
 
     """
+        se for -1 ignorar
         processors(requested) * avg CPU time * const = CPU_profile
         com = 0
     """
